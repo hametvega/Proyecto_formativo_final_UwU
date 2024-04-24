16608c389ea415517afcbc14d951f17519d72a94
hametvega <hamet2303@gmail.com>
2024-04-24 06:53:46 -0500

    Reinicializando el repositorio para no tener problemas con las ramas

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, request, url_for,flash, session
+
+app = Flask(__name__)
+app.secret_key = 'Dani_Server';
+
+#Definir rutas
+@app.route('/')
+def Pagina_Principal():
+    
+    print('This is a route. :)')
+    
+    return render_template('Index.html');
+
+@app.route('/Ayuda_Agraria')
+def Pagina_De_Ayuda():
+
+    print('This is another route. :/')
+
+    return render_template('Agro_Help.html');
+
+@app.route('/Productos')
+def Pagina_De_Productos():
+
+    print('This is another route. :D')
+
+    return render_template('Registro_Product.html');
