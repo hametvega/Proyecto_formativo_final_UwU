97cee3412c47964b8d9957708b385cfa352b27c8
IrmaRios19 <irma4x4@hotmail.com>
2024-04-26 06:47:33 -0500

    Agregelos cambios de Daniel a mi rama.

--- main.py
+++ main.py
@@ -44,24 +44,44 @@ def Pagina_De_Info_Extra():
 @app.route('/Registro_personas', methods=['GET', 'POST'])
 def Registrando_p():
         if request.method == 'POST': #si el metodo es GET si suelta error
-            nombre_p = request.form.get['txtNombre']
-            apellido_p = request.form.get['txtApellido']
-            celular_p = request.form.get['txtCelular']
-            direccion_p = request.form.get['txtDireccion']
-            correo_p = request.form.get['txtEmail']
-            contraseña_p = request.form.get['txtContraseña']
+            nombre_p = request.form.get['name']
+            apellido_p = request.form.get['Lastname']
+            telefono_p = request.form.get['telefono']
+            correo_p = request.form.get['Email']
+            direccion_p = request.form.get['Adress']
+            contraseña_p = request.form.get['Password']
 
             # Validar que todos los campos estén llenos
-            if not (nombre_p and apellido_p and celular_p and direccion_p and correo_p and contraseña_p):
-                return render_template('Pagina_De_Registro_E_Inicio_Sesion.html', error='Por favor, complete todos los campos del formulario.')
+            if not (nombre_p and apellido_p and telefono_p and direccion_p and correo_p and contraseña_p):
+                return render_template('registrar.html', error='Por favor, complete todos los campos del formulario.')
 
             try:
                 cursor.execute("INSERT INTO registro_personas (Nombre_persona, Apellido_person, Celular, Dirección, Email, Contraseña) VALUES (%s, %s, %s, %s, %s, %s)", 
-                            (nombre_p, apellido_p, celular_p, direccion_p, correo_p, contraseña_p))
+                            (nombre_p, apellido_p, telefono_p, direccion_p, correo_p, contraseña_p))
                 db.commit()
                 return redirect(url_for('Registrando_p'))
             except mysql.connector.errors.IntegrityError as e:
-                return render_template('Pagina_De_Registro_E_Inicio_Sesion.html', error=str(e))
+                return render_template('registrar.html', error=str(e))
         
         return render_template('Login_Registro.html')
 #Los datos no se guardan en la tabla sin embargo no suelta error 
+
+##
+
+# @app.route('/Registro_personas', methods=['GET', 'POST'])
+# def Registrando_p():
+    
+#     if request.method == 'GET':
+#         nombre_p = request.form.get["Name"]
+#         apellido_p = request.form.get['Lastname']
+#         telefono_p = request.form.get['telefono']
+#         correo_p = request.form.get['Email']
+#         direccion_p = request.form.get['Adress']
+#         contraseña_p = request.form.get['Password']
+
+#         cursor.execute("Insert Into registro_personas (Nombre_persona, Apellido_person, Celular, Dirección, Email, Contraseña) Values (%s, %s, %s, %s, %s, %s)", 
+#                                 (nombre_p, apellido_p, telefono_p, direccion_p, correo_p, contraseña_p))
+#         db.commit()
+#         return redirect(url_for('Registrando_p'))
+    
+#     return render_template('registrar.html')
